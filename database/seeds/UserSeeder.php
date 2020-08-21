14d1d8ef6b3ee753d540611965942c04ce583944
bronkopopovic <romanhock91@googlemail.com>
2020-08-21 20:32:48 +0200

    add admin user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = (new \DateTime())->format('Y-m-d H:i:s');
+
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+    }
+}
